cbe697c736debe424cd68e3c693be6c56bfabe3d
R3on322 <kindeer123@mail.ru>
2022-07-02 14:49:18 +0300

    [UPD] Added more regex to script

--- Regex_email_phone.py
+++ Regex_email_phone.py
@@ -2,6 +2,11 @@ import re
 from randomtimestamp import randomtimestamp
 import time
 
+# def chek_phone(number):
+#     num_ok = re.match(r'([0-9]{3})([\d-]{7})', number)
+#     print(num_ok.group())
+# num = 9162256413
+# chek_phone(str(num))
 
 def chek_mail(email):
     try:
@@ -14,26 +19,18 @@ def chek_mail(email):
 with open('patch.txt', 'r') as file:
     data = file.readlines()
 new_data = []
-for i in data:
-    list_string = re.split(r'\s', i.rstrip('\n'))
-    if 'From:' in list_string:
-        list_string = ['From:', ]
-        new_name = input('Enter your name: ')
-        new_email = input('Enter your email address: ')
-        list_string.append(new_name)
-        while chek_mail(new_email) == False:
-            new_email = input('Wrong email, pls enter correct email.(Example: "abs123@gmail.com")\n')
-        else:
-            list_string.append(f'<{new_email}>')
-    if 'Date:' in list_string:
-        list_string = ['Date:', ]
-        random_date = randomtimestamp(text=False).strftime(f"%a, %d %b %Y %H:%M:%S {time.strftime('%z')}")
-        list_string.append(random_date)
-    if 'Subject:' in list_string:
-        list_string = ['Subject:', ]
-        new_subject = input('Enter your subject: ')
-        list_string.append(new_subject)
-    new_data.append(' '.join(list_string))
-
+new_name = input('Enter your name: ')
+new_email = input('Enter your email address: ')
+while chek_mail(new_email) == False:
+    new_email = input('Wrong email, pls enter correct email.(Example: "abs123@gmail.com")\n')
+new_subject = input("Enter your subject: ")
+random_date = randomtimestamp(text=False).strftime(f"%a, %d %b %Y %H:%M:%S {time.strftime('%z')}")
+for string in data:
+    string = re.sub(r'(From:)(.+)(.+)$', fr'\1 {new_name} <{new_email}>', string)
+    string = re.sub(r'(Date:) (.+)$', fr'\1 {random_date}', string)
+    string = re.sub(r'(Subject:) (.+)$', fr'\1 {new_subject}', string)
+    new_data.append(string)
+for i in new_data:
+    print(i.strip('\n'))
 with open('patch.txt', 'w') as file:
-    file.write('\n'.join(new_data))
+    file.write(''.join(new_data))
