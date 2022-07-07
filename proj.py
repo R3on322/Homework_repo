bb7b693dfeeb4dfc6640e35080985ddf993c4f99
R3on322 <kindeer123@mail.ru>
2022-07-07 12:06:51 +0300

    [UPD] Update def merge_with_other

--- proj.py
+++ proj.py
@@ -22,15 +22,19 @@ class Stack:
         return self.data
 
     def merge_with_other(self, other_stack: 'Stack'):
+        empty_stack = []
         if len(self.data) > len(other_stack):
             for index_elem in range(len(other_stack)):
-                self.data[index_elem] = self.data[index_elem] + other_stack[index_elem]
+                empty_stack.append(self.data[index_elem])
+                empty_stack.append(other_stack[index_elem])
+            self.data = empty_stack
         else:
             for index_elem in range(len(self.data)):
-                other_stack[index_elem] = self.data[index_elem] + other_stack[index_elem]
-            self.data = other_stack
+                empty_stack.append(self.data[index_elem])
+                empty_stack.append(other_stack[index_elem])
+            self.data = empty_stack
 
-other_stack = ['6', '7', '1', '2']
+other_stack = ['6', '7', '1', '6', '7', '8']
 stack = Stack()
 stack.push('a')
 stack.push('b')
